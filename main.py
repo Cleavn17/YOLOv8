0169ffbcc317445cb527cdc4fc5aadc5725f2669
Cleavon Castelino <ccasteli@tcd.ie>
2024-05-29 17:42:11 +0100

    Changed path

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from ultralytics import YOLO
 
 # Create a new YOLO model from scratch
-model = YOLO('yolov8m.yaml')
+model = YOLO('yolov8s.yaml')
 
 # Train the model using the 'coco8.yaml' dataset for 3 epochs
-results = model.train(data='config.yaml', epochs=2)
+results = model.train(data='dataset.yaml', epochs=50)
